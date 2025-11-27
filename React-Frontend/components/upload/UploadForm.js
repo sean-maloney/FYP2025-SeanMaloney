18b3893b4ce688fe8b5228ecd046ca295fc863ee
Sean Maloney <G00420811@atu.ie>
2025-11-27 15:37:45 +0000

    I think upload is done, added some css will comment next

--- React-Frontend/components/upload/UploadForm.js
+++ React-Frontend/components/upload/UploadForm.js
@@ -22,5 +22,55 @@ function UploadForm() {
             setMessage("Please choose a video file first.");
             return;
         }
+
+        setIsUploading(true);
+        setMessage("");
+
+        try {
+            const body = new FormData();
+            body.append("file", video);
+
+            const response = await fetch(`${API_BASE}/api/videos`, {method: "POST", body:body,});
+
+            if(!response.ok){
+                const errData = await response.json().catch(() => ({}));
+                throw new Error(errData.detail || "Upload failed");
+            }
+
+            const data = await response.json();
+            setJobId(data.job_id);
+            setMessage(data.message || "Upload complete.");
+            }
+            catch (error) {
+                console.error(error);
+                setMessage(`Error : ${error.message}`);
+            }
+            finally{
+                setIsUploading(false);
+            }
     }
+
+    return(
+        <section>
+            <form onSubmit={submitHandler} style={{marginTop: "5px"}}>
+                <div style={{ marginBottom: "4px"}}>
+                    <input type="file" accept ="video/*" onChange={fileChangeHandler}/>
+                </div>
+                
+                <button type="submit" disabled={isUploading}>
+                    {isUploading ? "Uploading..." : "Upload & Process"} 
+                </button>
+            </form>
+
+                {message && <p style={{ marginTop: "5px"}}>{message}</p>}
+                {jobID && (
+                    <p style={{ marginTop: "3px"}}>
+                        View Processed video:{" "}
+                        <a href={`/view/${jobId}`}>click here</a>
+                    </p>
+                )}
+        </section>
+    );
 }
+
+export default UploadForm;
